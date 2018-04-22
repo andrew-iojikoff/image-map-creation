eec8c02af78a5001c8555867dc6e2d3b4d6401dc
andrew-iojikoff <andrew.ejov@gmail.com>
2018-04-22 22:41:40 +0300

    add image loading

--- src/components/EditeMap.vue
+++ src/components/EditeMap.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>dddddddddddddddddddddddddddddddd</div>
+  <div>
+    <img v-bind:src=" imageSrc " alt="Изображение для редактирования">
+  </div>
 </template>
 <script>
+  import router from '../router'
 
+  export default {
+    name: 'EditeMap',
+    methods: {
+
+    },
+    computed: {
+       imageSrc: function () {
+           return this.$store.state.img;
+       }
+    },
+    data () {
+      return {
+          img: ''
+      }
+    }
+  }
 </script>
 <style>
 
